45932cb5aeec5561937ef894ad59901a806ffffe
Remco <remcodebruin@gmx.net>
2015-11-18 18:39:07 +0100

    server auto-reloads

--- src/clj/duck/server.clj
+++ src/clj/duck/server.clj
@@ -5,11 +5,13 @@
         [compojure.route :refer [not-found resources]]
         [compojure.handler :refer [site]] ; ??? form, query params decode; cookie; session, etc
         [hiccup.core :refer [html]]
-        [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+        [ring.middleware.reload :as reload]
+        [ring.middleware.defaults :refer [site-defaults wrap-defaults api-defaults]]
         [ring.middleware.json :refer [wrap-json-response]]
         [ring.util.response :refer [response]]))
 
 
+;this should be in the cljs part
 (defn test-page [req]
   (html
    [:html [:body [:form {:method "post" :action "/javadoc"}
@@ -20,12 +22,13 @@
   (response {:foo (str "echo: " url)}))
 
 (defroutes routes
+  (GET "/" [] (slurp "resources/public/index.html"))
   (GET "/test" [] test-page)
   (POST "/javadoc" [url] (do
                            (println "git url: " url)
-                           (route-javadoc url))))
+                           (route-javadoc url)))
+  (resources "/"))
 
 (def handler
-  (-> routes
-      (wrap-defaults (assoc site-defaults :security false)) ; remove anti-forgery stuff
-      wrap-json-response))
+    (reload/wrap-reload (wrap-defaults #'routes api-defaults)))
+
