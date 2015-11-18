b12263a6d89c4b0b6214abe0b6fb0ec9dbdcad16
Remco <remcodebruin@gmx.net>
2015-11-18 20:42:34 +0100

    validate github url with red/green color

--- src/cljs/duck/core.cljs
+++ src/cljs/duck/core.cljs
@@ -10,7 +10,8 @@
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (reagent/atom
                      {:header-title "Hello world!"
-                      :javadoc-response ""}))
+                      :javadoc-response ""
+                      :github-url"https://github.com/pallix/tikkba"}))
 
 (defn get-json-javadoc [git-url success]
   (POST "/javadoc"
@@ -22,17 +23,30 @@
                          (.log js/console err))}))
 
 (defn generate-doc-click [& e]
-  (get-json-javadoc "https://github.com/weavejester/ring-json-response"
+  (get-json-javadoc (@app-state :github-url) ;"https://github.com/weavejester/ring-json-response"
                     (fn [data]
                       (this-as my-this (.log js/console my-this))
                       (.log js/console e)
                       (swap! app-state assoc :javadoc-response (:foo data)))))
 
+
+(defn github-url? [url]
+  (re-matches #"https?://github.com/.*/.*/?" url))
+
+;change this to class names
+(defn github-url->color [url]
+  (if-not (nil? (github-url? url))
+    "green"
+    "red"))
+
 (defn main-page []
   [:div
    [:h1 (:header-title @app-state)]
    [:div
-    "Git url " [:input {:id "git-url" :type "text"}] "  "
+    "Git url "
+    [:input {:id "git-url" :type "text" :value (@app-state :github-url) :style {:background-color (github-url->color (@app-state :github-url))}
+             :on-change (fn [x] (swap! app-state assoc :github-url (-> x .-target .-value) ))}] "  "
+
     [:span {:class "button" :on-click generate-doc-click} "Create"]]
    [:div
     [:h2 "Server response"]
@@ -43,8 +57,6 @@
 
 
 (reagent/render-component [main-page] (. js/document (getElementById "app")))
-
-
 
 
 
