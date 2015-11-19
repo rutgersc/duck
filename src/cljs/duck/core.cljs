d55a5975bed575a6a947f8f2188663f17b99337f
Rutger <schoorstra_rutger@hotmail.com>
2015-11-19 13:52:43 +0100

    Added pause-resume & test button

--- src/cljs/duck/core.cljs
+++ src/cljs/duck/core.cljs
@@ -14,6 +14,7 @@
                      {:header-title "Duck Project!"
                       :javadoc-response ""
                       :github-url "https://github.com/pallix/tikkba"
+                      :paused false
                       :width 500
                       :height 500}))
 
@@ -26,14 +27,6 @@
          :error-handler (fn [err]
                          (.log js/console err))}))
 
-(defn generate-doc-click [& e]
-  (get-json-javadoc (@app-state :github-url) ;"https://github.com/weavejester/ring-json-response"
-                    (fn [data]
-                      (this-as my-this (.log js/console my-this))
-                      (.log js/console e)
-                      (swap! app-state assoc :javadoc-response (:foo data)))))
-
-
 (defn github-url? [url]
   (re-matches #"https?://github.com/.*/.*/?" url))
 
@@ -42,14 +35,32 @@
     "incorrect-url"
     "correct-url"))
 
+(defn set-quil-pause! [pause]
+  (swap! app-state assoc :paused pause)
+  (quil-core/with-sketch (quil-core/get-sketch-by-id "javadoc-canvas")
+    (if pause
+      (quil-core/no-loop)
+      (quil-core/start-loop))))
+
 (quil-core/defsketch javadoc-sketch
-  :host "javadoc-canvas"
-  :size [(.-innerWidth  js/window) (.-innerHeight  js/window)]
-  :setup  duck-quil/setup
-  :update duck-quil/update-state
-  :draw   duck-quil/draw-state
-  :navigation-2d {}
-  :middleware [m/fun-mode m/navigation-2d])
+              :host "javadoc-canvas"
+              :size [(.-innerWidth  js/window) (.-innerHeight  js/window)]
+              :setup  duck-quil/setup
+              :update duck-quil/update-state
+              :draw   duck-quil/draw-state
+              :navigation-2d {}
+              :middleware [m/fun-mode m/navigation-2d])
+
+
+(defn generate-doc-click [& e]
+  (get-json-javadoc (@app-state :github-url) ;"https://github.com/weavejester/ring-json-response"
+                    (fn [data]
+                      (this-as my-this (.log js/console my-this))
+                      (.log js/console e)
+                      (swap! app-state assoc :javadoc-response (:foo data)))))
+
+(defn test-click [e]
+  (.log js/console javadoc-sketch))
 
 (defn main-page []
   [:div
@@ -64,9 +75,9 @@
                  :on-change (fn [x] (swap! app-state assoc :github-url (-> x .-target .-value)))}]
         [:a {:href "#" :class "button" :on-click generate-doc-click} "Create"]]
       [:div
-        (:javadoc-response @app-state)]]])
-
-
+        (:javadoc-response @app-state)]
+      [:a {:href "#" :class "button" :on-click #(set-quil-pause! (not (@app-state :paused)))} "Pause/resume"]
+      [:a {:href "#" :class "button" :on-click test-click} "test"]]])
 
 (reagent/render-component [main-page] (. js/document (getElementById "app")))
 
@@ -79,6 +90,6 @@
 (defonce on-first-load
   (do
     (println "First load")
-    (.addEventListener js/window "resize" #(windowresize-handler))))
+    (.addEventListener js/window "resize" windowresize-handler)))
 
 (defn on-js-reload [])
