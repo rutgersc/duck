c7e92426d9a7f5794902d5db013f3a429ada704b
Rutger <schoorstra_rutger@hotmail.com>
2015-11-19 15:27:18 +0100

    Doclet prints all packages + all classes

--- src/clj/duck/doclet.clj
+++ src/clj/duck/doclet.clj
@@ -5,6 +5,17 @@
       :main false
       :methods [^:static [start [com.sun.javadoc.RootDoc] boolean]]))
 
+(defn print-package-name [package]
+  (println " " (.name package)))
+
+(defn all-classes [packages]
+  (mapcat (fn [p] (.ordinaryClasses p)) packages))
+
 (defn -start [root]
-    (println "Hello, javadoc!!")
-    true)
+  (let [packages (-> root .specifiedPackages)]
+    (println "All packages:")
+    (doall (map print-package-name packages))
+    (println "All classes:")
+    (let [classes (all-classes packages)]
+      (doall (map (fn [c] (println " " (.name c))) classes))))
+  true)
